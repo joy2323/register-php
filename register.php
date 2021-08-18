7e171807b63fcda781cc471be1d95efd66cb809e
joy2323 <joyjoel2011@gmail.com>
2021-08-18 15:02:17 +0100

    first commit

---
+++ register.php
@@ -0,0 +1,47 @@
+<? include('server.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration & Login With PHP and MYSql</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <h2>
+            Register
+        </h2>
+    </div>
+
+
+    <form action="register.php" method="post">
+        <? include('errors.php'); ?>
+        <div class="input-group">
+            <label for="">Username</label>
+            <input type="text" name="username" value="<?php echo $username; ?>">
+        </div>
+        <div class="input-group">
+            <label for=""> Email </label>
+            <input type="text" name="email" value="<?php echo $email; ?>">
+        </div>
+        <div class="input-group">
+            <label>Password</label>
+            <input type="password" name="password_1">
+        </div>
+        <div class="input-group">
+            <label>Confirm password</label>
+            <input type="password" name="password_2">
+        </div>
+        <div class="input-group">
+            <button type="submit" class="btn" name="reg_user">Register</button>
+        </div>
+        <p>
+            Already a member? <a href="login.php">Sign in</a>
+        </p>
+    </form>
+
+
+</body>
+</html>
